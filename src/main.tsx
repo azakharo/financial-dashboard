30ebebd6696f617bcd4bde5462453ccb0b5e0ea2
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-06-17 16:06:26 +0300

    added react-query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {QueryProvider, AppRouter} from '@/app';
 import {Agentation} from 'agentation';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryProvider>
       <AppRouter />
+
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
     </QueryProvider>
     <Agentation />
   </StrictMode>,
