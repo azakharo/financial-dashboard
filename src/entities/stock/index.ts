27b1a24ba4e491cba35f067c92b2a158efcbde38
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-07-29 16:26:27 +0300

    rem unnec code

--- src/entities/stock/index.ts
+++ src/entities/stock/index.ts
@@ -1,9 +1,2 @@
 export {useStocks} from './api';
 export {stocksQueryKey} from './queryKeys';
-export {
-  selectStockByTicker,
-  selectTotalPortfolioValue,
-  selectStocksBySector,
-  selectStocksSortedByTicker,
-  selectStocksSortedByPrice,
-} from './model';
